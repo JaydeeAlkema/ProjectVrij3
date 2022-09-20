19e4a134c0f400159d071d9c6d6e0a819d36ff81
joshua <joshuawiranta@hotmail.com>
2022-09-20 17:22:54 +0200

    ability scriptable workon

--- Assets/Scripts/Player/AbilityScriptable.cs
+++ Assets/Scripts/Player/AbilityScriptable.cs
@@ -19,6 +19,12 @@ public class AbilityScriptable : ScriptableObject
 	private EdgeCollider2D shape;
 	[SerializeField]
 	private float distance = 0f;
+	[SerializeField]
+	private Vector2 boxSize = new Vector2( 4, 6 );
+	[SerializeField]
+	private float circleSize = 0f;
+	[SerializeField]
+	private LayerMask layerMask;
 
 	private void DealDamage(GameObject target)
 	{
@@ -29,5 +35,36 @@ public class AbilityScriptable : ScriptableObject
 	{
 		//this.rb2d.velocity += movement;
 	}
-	
+
+	public void LineUp(Rigidbody2D player, Transform castFromPoint, float angle, Vector2 lookDir)
+	{
+		Collider2D[] enemiesInBox = Physics2D.OverlapBoxAll( player.transform.position + castFromPoint.transform.up * 3, boxSize, angle, layerMask );
+		Debug.Log( "Enemies: " + enemiesInBox.Length );
+
+		foreach( Collider2D enemy in enemiesInBox )
+		{
+			Vector3 abNormal = lookDir.normalized;
+			Vector3 enemyVec = enemy.transform.position - player.transform.position;
+
+			float dotP = Vector2.Dot( enemyVec, abNormal );
+
+			Vector3 newPoint = player.transform.position + ( abNormal * dotP );
+			enemy.GetComponent<ICrowdControllable>()?.Pull( newPoint );
+		}
+	}
+
+	public void BlackHole( Rigidbody2D player, Transform castFromPoint, float angle, Vector2 lookDir )
+	{
+		Vector2 circlePos = player.transform.position + castFromPoint.transform.up * 5;
+		Collider2D[] enemiesInCircle = Physics2D.OverlapCircleAll( circlePos, circleSize, layerMask );
+		Debug.Log( "Enemies: " + enemiesInCircle.Length );
+
+		foreach( Collider2D enemy in enemiesInCircle )
+		{
+			Vector3 newPoint = circlePos;
+			enemy.GetComponent<ICrowdControllable>()?.Pull( newPoint );
+		}
+
+	}
+
 }
--- Assets/Scripts/Player/ScuffedDesignPrototypes/LineUpTest.cs
+++ Assets/Scripts/Player/ScuffedDesignPrototypes/LineUpTest.cs
@@ -11,7 +11,7 @@ public class LineUpTest : MonoBehaviour
 	public Vector3 newPoint;
 	public Vector3 mousePos;
 	public Camera cam;
-	public GameObject hands;
+	public GameObject CastFromPoint;
 	public float angle;
 	public Vector2 boxSize = new Vector2(4, 6);
 	public float circleSize = 3f;
@@ -40,12 +40,12 @@ public class LineUpTest : MonoBehaviour
 		mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 		lookDir = mousePos - player.transform.position;
 		angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-		hands.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+		CastFromPoint.transform.rotation = Quaternion.Euler(0f, 0f, angle);
 	}
 
 	public void LineUp()
 	{
-		Collider2D[] enemiesInBox = Physics2D.OverlapBoxAll(player.transform.position + hands.transform.up * 3, boxSize, angle, layerMask);
+		Collider2D[] enemiesInBox = Physics2D.OverlapBoxAll(player.transform.position + CastFromPoint.transform.up * 3, boxSize, angle, layerMask);
 		Debug.Log("Enemies: " + enemiesInBox.Length);
 
 		foreach (Collider2D enemy in enemiesInBox)
@@ -62,7 +62,7 @@ public class LineUpTest : MonoBehaviour
 
 	public void BlackHole()
 	{
-		Vector2 circlePos = player.transform.position + hands.transform.up * 5;
+		Vector2 circlePos = player.transform.position + CastFromPoint.transform.up * 5;
 		Collider2D[] enemiesInCircle = Physics2D.OverlapCircleAll(circlePos, circleSize, layerMask);
 		Debug.Log("Enemies: " + enemiesInCircle.Length);
 
@@ -77,10 +77,10 @@ public class LineUpTest : MonoBehaviour
 	public void OnDrawGizmos()
 	{
 		Gizmos.color = Color.blue;
-		Gizmos.matrix = Matrix4x4.TRS(player.transform.position + hands.transform.up * 3, hands.transform.rotation, boxSize);
+		Gizmos.matrix = Matrix4x4.TRS(player.transform.position + CastFromPoint.transform.up * 3, CastFromPoint.transform.rotation, boxSize);
 		Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
 		Gizmos.color = Color.red;
-		Gizmos.matrix = Matrix4x4.TRS(player.transform.position + hands.transform.up * 5, hands.transform.rotation, new Vector3(circleSize, circleSize, 0));
+		Gizmos.matrix = Matrix4x4.TRS(player.transform.position + CastFromPoint.transform.up * 5, CastFromPoint.transform.rotation, new Vector3(circleSize, circleSize, 0));
 		Gizmos.DrawWireSphere(Vector3.zero, 1);
 	}
 }
