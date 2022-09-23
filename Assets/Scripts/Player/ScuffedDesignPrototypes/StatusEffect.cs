cbf7dcecade93970e3c77ceeb09890edbb70aa5c
Max <maxdwilde13@gmail.com>
2022-09-23 10:46:46 +0200

    Add StatusEffect class + StatusEffect_Burning

---
+++ Assets/Scripts/Player/ScuffedDesignPrototypes/StatusEffect.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StatusEffect : MonoBehaviour
+{
+	//JOSHUA COMMENT DIT WEG PLS:
+	//public IDamageable();
+
+	public virtual void Process()
+	{
+		//lmao
+	}
+
+}
---
+++ Assets/Scripts/Player/ScuffedDesignPrototypes/StatusEffect_Burning.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusEffect_Burning : StatusEffect
+{
+	public override void Process()
+	{
+		
+	}
+}
