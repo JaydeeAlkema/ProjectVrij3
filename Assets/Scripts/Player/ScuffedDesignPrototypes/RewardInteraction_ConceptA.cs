dc097cd5dc449811b4dcb54245275308c7f7900a
Max <maxdwilde13@gmail.com>
2022-09-26 13:30:01 +0200

    Add reward interaction concept Dummy A

--- Assets/Scripts/Player/ScuffedDesignPrototypes/RewardInteraction_ConceptA.cs
+++ Assets/Scripts/Player/ScuffedDesignPrototypes/RewardInteraction_ConceptA.cs
@@ -7,11 +7,24 @@ public class RewardInteraction_ConceptA : MonoBehaviour
 {
 	public Transform popup;
 
+	private void Awake()
+	{
+		popup.gameObject.SetActive(false);
+	}
+
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.CompareTag("Player"))
 		{
-
+			popup.gameObject.SetActive(true);
+		}
+	}	
+	
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			popup.gameObject.SetActive(false);
 		}
 	}
 }
