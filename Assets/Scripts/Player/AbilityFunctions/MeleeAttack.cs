9e116d501df4f83146baf67f85f51696fcd1b407
Joshua <joshuawiranta@hotmail.com>
2022-09-30 15:45:28 +0200

    cooldown

--- Assets/Scripts/Player/AbilityFunctions/MeleeAttack.cs
+++ Assets/Scripts/Player/AbilityFunctions/MeleeAttack.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class MeleeAttack : Ability
 {
+	private bool onCoolDown = false;
 	public override void AbilityBehavior()
 	{
-		Collider2D[] enemiesInBox = Physics2D.OverlapBoxAll( rb2d.transform.position + castFromPoint.transform.up * distance, boxSize, angle, layerMask );
-		Debug.Log( "Enemies: " + enemiesInBox.Length );
-
-		foreach( Collider2D enemy in enemiesInBox )
+		if( !onCoolDown )
 		{
-			enemy.GetComponent<IDamageable>()?.TakeDamage( damage );
+			StartCoroutine( CoolDown() );
+			Collider2D[] enemiesInBox = Physics2D.OverlapBoxAll( rb2d.transform.position + castFromPoint.transform.up * distance, boxSize, angle, layerMask );
+			Debug.Log( "Enemies: " + enemiesInBox.Length );
+
+			foreach( Collider2D enemy in enemiesInBox )
+			{
+				enemy.GetComponent<IDamageable>()?.TakeDamage( damage );
+			}
 		}
 	}
 
-	public void SetAbilityStats( Rigidbody2D AbilityRB2D, Transform CastFromPoint, Vector2 BoxSize, Vector2 LookDir, float Angle, LayerMask layer, float Damage, float Distance )
+	public void SetAbilityStats( Rigidbody2D AbilityRB2D, Transform CastFromPoint, Vector2 BoxSize, Vector2 LookDir, float Angle, LayerMask layer, float Damage, float Distance, float Cooldown )
 	{
 		rb2d = AbilityRB2D;
 		castFromPoint = CastFromPoint;
@@ -25,11 +30,19 @@ public class MeleeAttack : Ability
 		lookDir = LookDir;
 		damage = Damage;
 		distance = Distance;
+		coolDown = Cooldown;
 	}
 
 	public override void SetScriptable( AbilityScriptable scriptable )
 	{
 		abilityScriptable = scriptable;
-		SetAbilityStats( scriptable.Rb2d, scriptable.CastFromPoint, scriptable.BoxSize, scriptable.LookDir, scriptable.Angle, scriptable.Layer, scriptable.Damage, scriptable.Distance );
+		SetAbilityStats( scriptable.Rb2d, scriptable.CastFromPoint, scriptable.BoxSize, scriptable.LookDir, scriptable.Angle, scriptable.Layer, scriptable.Damage, scriptable.Distance, scriptable.CoolDown );
+	}
+
+	IEnumerator CoolDown()
+	{
+		onCoolDown = true;
+		yield return new WaitForSeconds(coolDown);
+		onCoolDown = false;
 	}
 }
