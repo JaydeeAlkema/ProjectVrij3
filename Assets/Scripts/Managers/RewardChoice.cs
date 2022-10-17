9bcd151f77a2f6091c42a59b5c901189afa40cac
Joshua <joshuawiranta@hotmail.com>
2022-10-17 15:15:20 +0200

    reward tiers with odds

--- Assets/Scripts/Managers/RewardChoice.cs
+++ Assets/Scripts/Managers/RewardChoice.cs
@@ -8,7 +8,14 @@ public class RewardChoice : MonoBehaviour
     [SerializeField] private UpgradeScriptable upgradeToGive;
     [SerializeField] private AbilityScriptable meleeAttack;
     [SerializeField] private AbilityScriptable rangedAttack;
-    private UpgradeScriptable[] upgrades;
+    [SerializeField] private int roll;
+    //chances are based on a roll of 1-1000000
+    [SerializeField] private int t1Chance = 1; //1-700000
+    [SerializeField] private int t2Chance = 700001; //700001-995000
+    [SerializeField] private int t3Chance = 995001; //995001-1000000
+    private UpgradeScriptable[] tier1Upgrades;
+    private UpgradeScriptable[] tier2Upgrades;
+    private UpgradeScriptable[] tier3Upgrades;
     private PlayerControler player;
     public AbilityScriptable AbilityToGive { get => abilityToGive; set => abilityToGive = value; }
     public UpgradeScriptable UpgradeToGive { get => upgradeToGive; set => upgradeToGive = value; }
@@ -16,13 +23,48 @@ public class RewardChoice : MonoBehaviour
 	private void Start()
 	{
         player = FindObjectOfType<PlayerControler>();
-        //load all upgrade assets
-        int maxRange = Resources.LoadAll<UpgradeScriptable>( "ScriptableObjects/Upgrade" ).Length;
-        upgrades = new UpgradeScriptable[maxRange];
-        upgrades = Resources.LoadAll<UpgradeScriptable>( "ScriptableObjects/Upgrades");
-        Debug.Log( "upgrades loaded: " + upgrades.Length );
-        upgradeToGive = upgrades[Random.Range( 0, upgrades.Length -1 )];
-        Debug.Log(upgradeToGive.name);
+        //load tier 1 upgrades
+        int maxRangeT1 = Resources.LoadAll<UpgradeScriptable>( "ScriptableObjects/Upgrades/Tier1" ).Length;
+        tier1Upgrades = new UpgradeScriptable[maxRangeT1];
+        tier1Upgrades = Resources.LoadAll<UpgradeScriptable>( "ScriptableObjects/Upgrades/Tier1");
+        Debug.Log( "tier 1 upgrades loaded: " + tier1Upgrades.Length );
+        //load tier 2 upgrades
+        int maxRangeT2 = Resources.LoadAll<UpgradeScriptable>( "ScriptableObjects/Upgrades/Tier2" ).Length;
+        tier2Upgrades = new UpgradeScriptable[maxRangeT2];
+        tier2Upgrades = Resources.LoadAll<UpgradeScriptable>( "ScriptableObjects/Upgrades/Tier2" );
+        Debug.Log( "tier 2 upgrades loaded: " + tier2Upgrades.Length );
+        //load tier 3 upgrades
+        int maxRangeT3 = Resources.LoadAll<UpgradeScriptable>( "ScriptableObjects/Upgrades/Tier3" ).Length;
+        tier3Upgrades = new UpgradeScriptable[maxRangeT3];
+        tier3Upgrades = Resources.LoadAll<UpgradeScriptable>( "ScriptableObjects/Upgrades/Tier3" );
+        Debug.Log( "tier 3 upgrades loaded: " + tier3Upgrades.Length );
+
+        //Rolling for upgrade all folders must contain atleast 1 upgrade
+        roll = Random.Range( 1, 1000001 );
+        if(roll >= t3Chance)
+        {
+            if( tier3Upgrades.Length > 0 )
+            {
+                upgradeToGive = tier3Upgrades[Random.Range( 0, tier3Upgrades.Length - 1 )];
+                Debug.Log( upgradeToGive.name );
+            }
+        }
+        else if( roll >= t2Chance && roll < t3Chance)
+        {
+            if( tier2Upgrades.Length > 0 )
+            {
+                upgradeToGive = tier2Upgrades[Random.Range( 0, tier2Upgrades.Length - 1 )];
+                Debug.Log( upgradeToGive.name );
+            }
+        }
+        else
+        {
+            if( tier1Upgrades.Length > 0 )
+            {
+                upgradeToGive = tier1Upgrades[Random.Range( 0, tier1Upgrades.Length - 1 )];
+                Debug.Log( upgradeToGive.name );
+            }
+        }
     }
 
 	public void ChooseAbility()
