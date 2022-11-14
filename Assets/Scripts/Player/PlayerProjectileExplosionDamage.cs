8b9a090180731d88113b52c530aaee14841989da
Max <maxdwilde13@gmail.com>
2022-11-14 15:46:36 +0100

    Projectile explosion hit detection only frame 1

--- Assets/Scripts/Player/PlayerProjectileExplosionDamage.cs
+++ Assets/Scripts/Player/PlayerProjectileExplosionDamage.cs
@@ -8,10 +8,8 @@ public class PlayerProjectileExplosionDamage : MonoBehaviour
 	[SerializeField] private LayerMask layerMask;
 	[SerializeField] private float radius = 0.2f;
 	[SerializeField] private int damage;
-	private List<Collider2D> enemyList = new List<Collider2D>();
 	private Collider2D[] enemiesInArea;
-	private bool hitDetecting = false;
-	
+
 
 	public IAbility CastedFrom { get => castedFrom; set => castedFrom = value; }
 	public int LayerMask { get => layerMask; set => layerMask = value; }
@@ -23,24 +21,11 @@ public class PlayerProjectileExplosionDamage : MonoBehaviour
 		transform.localScale = new Vector3(radius, radius, radius);
 	}
 
-	void Update()
+	void Start()
 	{
 		enemiesInArea = Physics2D.OverlapCircleAll(transform.position, radius * 6, layerMask);
 		foreach (Collider2D enemy in enemiesInArea)
 		{
-			if (!enemyList.Contains(enemy))
-			{
-				enemyList.Add(enemy);
-				DamageEnemiesInAoE(enemy);
-			}
-		}
-	}
-
-	void DamageEnemiesInAoE(Collider2D enemy)
-	{
-		if (enemyList != null)
-		{
-			Debug.Log("Damaged enemy with: " + damage);
 			enemy.GetComponent<IDamageable>()?.TakeDamage(damage, 1);
 			castedFrom.OnHitApplyStatusEffects(enemy.gameObject.GetComponent<IDamageable>());
 		}
