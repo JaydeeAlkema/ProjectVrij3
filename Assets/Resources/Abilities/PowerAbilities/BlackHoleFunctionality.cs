671109587720e8d02490fbea4482fa4ef7abafd6
Joshua <joshuawiranta@hotmail.com>
2022-11-22 15:56:39 +0100

    Blackhole rework start

---
+++ Assets/Resources/Abilities/PowerAbilities/BlackHoleFunctionality.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleFunctionality : MonoBehaviour
+{
+	private float circleRadius;
+	private LayerMask layerMask;
+
+	public float CircleRadius { get => circleRadius; set => circleRadius = value; }
+	public LayerMask LayerMask { get => layerMask; set => layerMask =  value ; }
+
+	private void Start()
+	{
+		Destroy( this.gameObject, 0.1f );
+		BlackHole();
+		//animation event
+	}
+
+	public void BlackHole()
+	{
+		Collider2D[] enemiesInCircle = Physics2D.OverlapCircleAll( transform.position, circleRadius, layerMask );
+		Debug.Log( "Enemies: " + enemiesInCircle.Length );
+
+		foreach( Collider2D enemy in enemiesInCircle )
+		{
+			Vector3 newPoint = transform.position;
+			enemy.GetComponent<ICrowdControllable>()?.Pull( newPoint );
+		}
+	}
+}
