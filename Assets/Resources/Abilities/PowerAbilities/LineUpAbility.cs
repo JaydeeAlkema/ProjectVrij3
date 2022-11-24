ae84a5d212f2c85b73d992d26bcb7427f3a3b723
Max <maxdwilde13@gmail.com>
2022-11-24 16:35:48 +0100

    Add reworked LineUp ability + base VFX, needs coil VFX lines

--- Assets/Resources/Abilities/PowerAbilities/LineUpAbility.cs
+++ Assets/Resources/Abilities/PowerAbilities/LineUpAbility.cs
@@ -6,6 +6,7 @@ public class LineUpAbility : Ability
 {
 	private bool init = true;
 	private GameObject lineUpObject;
+	private Transform player;
 	public override void CallAbility(PlayerControler _playerControler)
 	{
 		if( init )
@@ -13,13 +14,14 @@ public class LineUpAbility : Ability
 			SetAbilityStats();
 			init = false;
 		}
+		player = _playerControler.transform;
 		AbilityBehavior();
 	}
 	
 	public override void AbilityBehavior()
 	{
-		GameObject lineUp = Object.Instantiate( lineUpObject, MousePos, CastFromPoint.rotation);
-		LineUpFunctionality lU = lineUp.GetComponent<LineUpFunctionality>();
+		GameObject lineUp = Object.Instantiate( lineUpObject, player.position, CastFromPoint.rotation * Quaternion.Euler(0, 0, -90));
+		LineUpFunctionality lU = lineUp.GetComponentInChildren<LineUpFunctionality>();
 		lU.BoxSize = boxSize;
 		lU.LookDir = LookDir;
 		lU.Angle = Angle;
