d7c0b27468b6c7f40c7dc94de49ae3f2c4b485ba
Max <maxdwilde13@gmail.com>
2022-12-06 17:07:56 +0100

    New burning ground

--- Assets/Scripts/Player/ScuffedDesignPrototypes/TrailWithTrigger.cs
+++ Assets/Scripts/Player/ScuffedDesignPrototypes/TrailWithTrigger.cs
@@ -8,39 +8,62 @@ public class TrailWithTrigger : MonoBehaviour
 
 	TrailRenderer fireTrail;
 	EdgeCollider2D trailCollider;
+
+	[SerializeField] private float timer = 0f;
+	[SerializeField] private float timeSpawnNew;
+	[SerializeField] private GameObject burningGround;
+
 	private int burnDamage;
 	public int BurnDamage { get => burnDamage; set => burnDamage = value; }
 
 	void Start()
 	{
-		fireTrail = this.GetComponent<TrailRenderer>();
+		//fireTrail = this.GetComponent<TrailRenderer>();
 
-		GameObject colliderGameObject = new GameObject("TrailCollider", typeof(EdgeCollider2D), typeof(OnTriggerStatusEffectApply));
-		trailCollider = colliderGameObject.GetComponent<EdgeCollider2D>();
-		trailCollider.isTrigger = true;
-		trailCollider.edgeRadius = 0.25f;
-		colliderGameObject.GetComponent<OnTriggerStatusEffectApply>().statusEffectType = StatusEffectType.Burn;
-		colliderGameObject.GetComponent<OnTriggerStatusEffectApply>().SetBurnValue( burnDamage );
+		//GameObject colliderGameObject = new GameObject("TrailCollider", typeof(EdgeCollider2D), typeof(OnTriggerStatusEffectApply));
+		//trailCollider = colliderGameObject.GetComponent<EdgeCollider2D>();
+		//trailCollider.isTrigger = true;
+		//trailCollider.edgeRadius = 0.25f;
+		//colliderGameObject.GetComponent<OnTriggerStatusEffectApply>().statusEffectType = StatusEffectType.Burn;
+		//colliderGameObject.GetComponent<OnTriggerStatusEffectApply>().SetBurnValue( burnDamage );
 	}
 
-	void Update()
+	void FixedUpdate()
 	{
-		SetTriggerPointsFromTrail(fireTrail, trailCollider);
-	}
+		//SetTriggerPointsFromTrail(fireTrail, trailCollider);
 
-	void SetTriggerPointsFromTrail(TrailRenderer trail, EdgeCollider2D collider)
-	{
-		List<Vector2> points = new List<Vector2>();
-		for(int position = 0; position<trail.positionCount; position++)
+		if (timer < timeSpawnNew)
 		{
-			points.Add(trail.GetPosition(position));
+			timer += Time.deltaTime;
 		}
-		collider.SetPoints(points);
+		else
+		{
+			SpawnBurningGround();
+			timer = 0f;
+		}
+
 	}
+
+	void SpawnBurningGround()
+	{
+		GameObject ground = Instantiate(burningGround, transform.position, Quaternion.identity);
+		ground.GetComponent<OnTriggerStatusEffectApply>().BurnDamage = burnDamage;
+	}
+
+	//void SetTriggerPointsFromTrail(TrailRenderer trail, EdgeCollider2D collider)
+	//{
+	//	List<Vector2> points = new List<Vector2>();
+	//	for(int position = 0; position<trail.positionCount; position++)
+	//	{
+	//		points.Add(trail.GetPosition(position));
+	//	}
+	//	collider.SetPoints(points);
+	//}
 
 	void OnDestroy()
 	{
-		if (trailCollider == null) return;
-		Destroy(trailCollider.gameObject);
+		SpawnBurningGround();
+		//if (trailCollider == null) return;
+		//Destroy(trailCollider.gameObject);
 	}
 }
