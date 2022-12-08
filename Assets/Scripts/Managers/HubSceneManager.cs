52bac569a41ebd23f6905e315c831c7854bbdd41
Joshua <joshuawiranta@hotmail.com>
2022-12-08 12:12:09 +0100

    reset branch

--- Assets/Scripts/Managers/HubSceneManager.cs
+++ Assets/Scripts/Managers/HubSceneManager.cs
@@ -5,15 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class HubSceneManager : MonoBehaviour
 {
-    public static HubSceneManager sceneManagerInstance { get; private set; }
+	public static HubSceneManager sceneManagerInstance { get; private set; }
 	private string loadScene;
 	private string lastScene;
-	private PlayerControler playerValues;
-	private PlayerControler player;
+	[SerializeField] private PlayerControler playerValues;
+	[SerializeField] private PlayerControler player;
 
 	private void Awake()
 	{
-		DontDestroyOnLoad(this);
+		DontDestroyOnLoad( this );
 		if( sceneManagerInstance != null && sceneManagerInstance != this )
 		{
 			Destroy( this );
@@ -24,16 +24,15 @@ public class HubSceneManager : MonoBehaviour
 		}
 	}
 
-	public void ChangeScene(string sceneToLoad, string currentScene)
+	public void ChangeScene( string sceneToLoad, string currentScene )
 	{
 		loadScene = sceneToLoad;
-		if((GameManager.Instance.currentGameState == GameState.Dungeon || GameManager.Instance.currentGameState == GameManager.Instance.lastGamestate) && GameManager.Instance.PlayerInstance != null)
-		{
-			if(GameManager.Instance.ScriptablePlayer != null) { GameManager.Instance.ScriptablePlayer = null; }
-			GameManager.Instance.ScriptablePlayer = ( ScriptablePlayer )ScriptableObject.CreateInstance( "ScriptablePlayer" );
-			GameManager.Instance.ScriptablePlayer.Player = GameManager.Instance.PlayerInstance.GetComponent<PlayerControler>();
-			playerValues = GameManager.Instance.ScriptablePlayer.Player;
-		}
+		if( GameManager.Instance.ScriptablePlayer != null ) { GameManager.Instance.ScriptablePlayer = null; }
+		GameManager.Instance.ScriptablePlayer = ( ScriptablePlayer )ScriptableObject.CreateInstance( "ScriptablePlayer" );
+		GameManager.Instance.ScriptablePlayer.Player = GameManager.Instance.PlayerInstance.GetComponent<PlayerControler>();
+		GameManager.Instance.ScriptablePlayer.AbilityController = GameManager.Instance.PlayerInstance.GetComponent<AbilityController>();
+		playerValues = GameManager.Instance.ScriptablePlayer.Player;
+
 		lastScene = currentScene;
 		SceneManager.UnloadSceneAsync( currentScene );
 		SceneManager.LoadSceneAsync( sceneToLoad, LoadSceneMode.Additive ).completed += HubSceneManager_completed;
@@ -44,7 +43,7 @@ public class HubSceneManager : MonoBehaviour
 	private void HubSceneManager_completed( AsyncOperation obj )
 	{
 		SceneManager.SetActiveScene( SceneManager.GetSceneByName( loadScene ) );
-		if( loadScene != "Hub Prototype" && lastScene != "Hub Prototype") { HoldPlayerOnSceneLoad(); }
+		if( GameManager.Instance.currentGameState == GameState.Dungeon && GameManager.Instance.lastGamestate == GameManager.Instance.currentGameState) { HoldPlayerOnSceneLoad(); }
 		Debug.Log( "hold player stats" );
 	}
 
@@ -59,51 +58,52 @@ public class HubSceneManager : MonoBehaviour
 		//PlayerControler player = FindObjectOfType<PlayerControler>().gameObject.GetComponent<PlayerControler>();
 		player = FindObjectOfType<PlayerControler>();
 		GameManager.Instance.PlayerInstance = player.gameObject;
+		//player.gameObject.GetComponent<AbilityController>() = GameManager.Instance.ScriptablePlayer.AbilityController;
 		player.PlayerAbilityController = playerValues.PlayerAbilityController;
 		player.PlayerAbilityController.Player = player;
 		player.CurrentMeleeAttack = playerValues.CurrentMeleeAttack;
 		player.CurrentMeleeAttack.Player = player;
 		player.CurrentMeleeAttack.Init = playerValues.CurrentMeleeAttack.Init;
-		player.MeleeAttackScr.SetHoldStats(playerValues.MeleeAttackScr);
+		player.MeleeAttackScr.SetHoldStats( playerValues.MeleeAttackScr );
 		player.CurrentMeleeAttack.Damage = playerValues.CurrentMeleeAttack.Damage;
 		player.CurrentRangedAttack = playerValues.CurrentRangedAttack;
 		player.CurrentRangedAttack.Player = player;
 		player.CurrentRangedAttack.Init = playerValues.CurrentRangedAttack.Init;
-		player.RangedAttackScr.SetHoldStats(playerValues.RangedAttackScr);
+		player.RangedAttackScr.SetHoldStats( playerValues.RangedAttackScr );
 		player.CurrentDash = playerValues.CurrentDash;
 		player.CurrentDash.Player = player;
 		player.CurrentDash.Init = playerValues.CurrentDash.Init;
-		player.Dash.SetHoldStats(playerValues.Dash);
-		if(playerValues.CurrentAbility1 != null)
-		{ 
+		player.Dash.SetHoldStats( playerValues.Dash );
+		if( playerValues.CurrentAbility1 != null )
+		{
 			player.CurrentAbility1 = playerValues.CurrentAbility1;
 			player.CurrentAbility1.Player = player;
 			player.Ability1 = playerValues.Ability1;
-			player.Ability1.SetHoldStats(playerValues.Ability1);
+			player.Ability1.SetHoldStats( playerValues.Ability1 );
 			player.CurrentAbility1.Init = playerValues.CurrentAbility1.Init;
 		}
-		if(playerValues.CurrentAbility2 != null)
+		if( playerValues.CurrentAbility2 != null )
 		{
 			player.CurrentAbility2 = playerValues.CurrentAbility2;
 			player.CurrentAbility2.Player = player;
 			player.Ability2 = playerValues.Ability2;
-			player.Ability2.SetHoldStats(playerValues.Ability2);
+			player.Ability2.SetHoldStats( playerValues.Ability2 );
 			player.CurrentAbility2.Init = playerValues.CurrentAbility2.Init;
 		}
-		if(playerValues.CurrentAbility3 != null)
+		if( playerValues.CurrentAbility3 != null )
 		{
 			player.CurrentAbility3 = playerValues.CurrentAbility3;
 			player.CurrentAbility3.Player = player;
 			player.Ability3 = playerValues.Ability3;
-			player.Ability3.SetHoldStats(playerValues.Ability3);
+			player.Ability3.SetHoldStats( playerValues.Ability3 );
 			player.CurrentAbility3.Init = playerValues.CurrentAbility3.Init;
 		}
-		if(playerValues.CurrentAbility4 != null)
+		if( playerValues.CurrentAbility4 != null )
 		{
 			player.CurrentAbility4 = playerValues.CurrentAbility4;
 			player.CurrentAbility4.Player = player;
 			player.Ability4 = playerValues.Ability4;
-			player.Ability4.SetHoldStats(playerValues.Ability4);
+			player.Ability4.SetHoldStats( playerValues.Ability4 );
 			player.CurrentAbility4.Init = playerValues.CurrentAbility4.Init;
 		}
 		//player.ReloadAttacks();
--- Assets/Scripts/ScriptableObjects/ScriptablePlayer.cs
+++ Assets/Scripts/ScriptableObjects/ScriptablePlayer.cs
@@ -6,4 +6,8 @@ public class ScriptablePlayer : ScriptableObject
 {
 	private PlayerControler player;
 	public PlayerControler Player { get => player; set => player = value; }
+
+	private AbilityController abilityController;
+
+	public AbilityController AbilityController { get => abilityController; set => abilityController = value; }
 }
